32d12ab28fac630ee97133fe6f6e04e4ebd72995
wsw70 <1345886+wsw70@users.noreply.github.com>
2021-08-25 16:56:05 +0200

    added Write and renamed Jsony to Read

--- jsony.go
+++ jsony.go
@@ -8,10 +8,11 @@ import (
 )
 
 var ErrReadingFile = errors.New("error reading file")
+var ErrWritingFile = errors.New("error writing file")
 var ErrIncorrectJson = errors.New("incorrect JSON")
 var ErrIncorrectStruct = errors.New("incorrect structure for JSON")
 
-func Jsony(filename string, expectedStructure interface{}) error {
+func Read(filename string, expectedStructure interface{}) error {
 	// error common to all function
 	var err error
 	// red in the file
@@ -31,4 +32,16 @@ func Jsony(filename string, expectedStructure interface{}) error {
 	}
 
 	return nil
+}
+
+func Write(data interface{}, filename string) error {
+	var err error
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("%w: %v", ErrIncorrectStruct, err)
+	}
+	err = os.WriteFile(filename, jsonData, 0600)
+	if err != nil {
+		return fmt.Errorf("%w: %v", ErrWritingFile, err)
+	}
 }
